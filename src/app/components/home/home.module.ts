c71538fa3550065b01423d621d35555fa0d33f95
root <root@ip-172-31-42-74.ec2.internal>
2020-04-08 23:31:35 +0000

    First commit

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+    HomeComponent
+  ]
+})
+export class HomeModule {}
